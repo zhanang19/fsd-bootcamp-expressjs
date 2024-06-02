841c5402006b38be1a019f3c17005cad55c070df
Zainal Hasan <19884603+zhanang19@users.noreply.github.com>
2024-06-02 12:54:55 +0700

    feat: init & configure cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
+const cors = require("cors");
 const express = require("express");
 const userRouter = require("./routes/user.router");
 const postRouter = require("./routes/post.router");
@@ -10,6 +11,20 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const whitelistOrigins = ["http://localhost:3000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelistOrigins.indexOf(origin) === -1) {
+        return callback(new Error("Not allowed by CORS"), false);
+      }
+
+      callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
